473e6c2bf2a3f3f46c93a06618d3a6966d24ce4b
Rohan Surana <rohansurana2810@gmail.com>
2021-03-22 08:26:16 +0530

    changes

--- Assignment3/proc_manager.c
+++ Assignment3/proc_manager.c
@@ -3,32 +3,34 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
-#include <time.h>
 #include <fcntl.h>
+#include <sys/types.h>
 
 
 int main(int argc, char *argv[]) {
 
-    if (argc < 2) { // If no files is entered print error
+    if (argc < 2) { // If no files are entered print error
         fprintf(stderr, "No files entered.\n"); // Error
         return 1;
     }
+
+    //initialize all variables
     char *line = NULL;
     size_t len = 0;
     ssize_t read;
     FILE *fp;
     char newString[20][20];
     int i, j, ctr;
-    // open file
+    int index = 1;
+    // open file of cmdfile.txt in read mode
     fp = fopen(argv[1], "r");
 
     if (fp == NULL) { // if file does not exist exit with an error
         printf("Can't open file.\n");
         exit(1);
     }
+    // loop to read and parse strings
     while ((read = getline(&line, &len, fp)) != -1) {
-        printf("---------------------------------------\n");
-        printf("Split string by space into words :\n");
         for (int i = 0; i < 20; i++)
             for (int j = 0; j < 20; j++)
                 newString[i][j] = 0;
@@ -45,12 +47,66 @@ int main(int argc, char *argv[]) {
                 j++;
             }
         }
-        printf("\n Strings or words after split by space are :\n");
+        // printing commands in cmdfile.txt
         for (i = 0; i < ctr; i++)
             printf(" %s\n", newString[i]);
     }
+    // close file
     fclose(fp);
-    if (line)
-        free(line);
-    exit(EXIT_SUCCESS);
+
+    pid_t pid;
+
+    // start fork process
+    pid = fork();
+    if (pid < 0) { // loop for if the fork fails
+        fprintf(stderr, "cannot fork");
+        exit(1);
+    } else if (pid == 0) { // child process
+        // memory allocation using malloc
+        char **argv = malloc(10 * sizeof(char *));
+        // get elements of the array
+        for (j = 0; j < 10; j++) {
+            argv[j] = (char *) 0;
+            break;
+        }
+        // *argv is the file being executed, argv is an array of char pointers
+        execvp(*argv, argv);
+        perror(*argv);
+        exit(1);
+    } else {  // parent process
+        // allocate characters to the out and error file
+        // sprintf send formatted output to a string pointed to
+        // outFile and errorFile is the pointer to array of char elements where string is stored
+
+        char outFile[20];
+        char errorFile[20];
+        sprintf(outFile, "%d.out", getpid());
+        sprintf(errorFile, "%d.err", getpid());
+        // code provided in connect2.c
+        int standardOutput = open(outFile, O_CREAT | O_RDWR | O_APPEND, 0777);
+        int errorOutput = open(errorFile, O_CREAT | O_RDWR | O_APPEND, 0777);
+        // creates a copy to the respective file descriptor
+        dup2(standardOutput, 1);
+        dup2(errorOutput, 2);
+
+        printf("Starting command %d: child %d pid of parent %d.\n", index, getpid(), getppid());
+        printf("Finished child %d pid of parent %d.\n", getpid(), getppid());
+        fflush(NULL);
+    }
+    index = index + 1;
+
+    // given code in lecture
+    // used to write the exit code or killed statement to the PID.err
+    int status;
+    while ((pid = wait(&status)) >= 0) {
+        if (pid > 0) {
+            // prints exit code of child
+            fprintf(stderr, "Exited with exitcode = %d\n", WEXITSTATUS(status));
+            // if process is killed prints kill signal
+            if (WIFSIGNALED(status)) {
+                fprintf(stderr, "Killed with signal %d\n", WTERMSIG(status));
+            }
+        }
+    }
+    return 0;
 }
